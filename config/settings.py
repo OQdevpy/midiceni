18f83bf390fc4f818ebc69652a62888abc0178fe
OQdev <o.qosimov@tuit.uz>
2023-05-21 04:43:20 +0500

    admin

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+# write static root
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("", include("apps.patient.urls")),
 ]
 
-if settings.DEBUG:
+
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
